cd8680cc43b575f8331419af443012df3812ea4d
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-11-08 16:59:10 +0000

    Updated scripts work with vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "https://feargh.github.io/postcode-react/",
     server: {
         port: 3000,
         strictPort: true,
